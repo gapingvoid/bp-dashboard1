e3170c337f23a443904ce86a3f6093dfdf998ddf
akorman128 <alexkorman@gmail.com>
2021-07-21 17:27:35 +0300

    >

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ if show_table:
     if denominator_q == "All" or filtered_df.shape == table.df.shape:
         prog_df = table.df
     else:
-        # get selected value from the question we're using as the denominator
-        denominator_sel = getattr(expander, denominator_q.lower())[0]
-        # get all data that used the selector
         try:
+            # get selected value from the question we're using as the denominator
+            denominator_sel = getattr(expander, denominator_q.lower())[0]
+            # get all data that used the selector
             prog_df = table.df[table.df[denominator_sel] == 1]
         except:
             st.warning("If selecting a category, you must also select a question from the category in the expander.")
